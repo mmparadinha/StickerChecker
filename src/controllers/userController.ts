762a581b05adb71170d511254ef3c67f502f49d7
mmparadinha <matheusmparadinha@gmail.com>
2022-11-11 21:22:15 -0300

    Add Joi validation, changes made to function return types

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -4,7 +4,7 @@ import bcrypt from 'bcrypt';
 import { v4 as uuid } from "uuid";
 import { searchUser, insertNewUser, checkSession, insertNewSession, removeUser, searchUserBySession, changeUsername, getUserStats } from "../repositories/userRepository.js";
 
-export async function createUser(req: Request, res: Response): Promise<Response<any, Record<string, any>>> {
+export async function createUser(req: Request, res: Response) {
     const username: string = stripHtml(req.body.username.trim()).result;
     const email: string = stripHtml(req.body.email.trim()).result;
     const password: string = stripHtml(req.body.password.trim()).result;
@@ -12,7 +12,10 @@ export async function createUser(req: Request, res: Response): Promise<Response<
 
     try {
         const emailExists = await searchUser(email);
-        if (emailExists.rows[0]) { return res.sendStatus(409); }
+        if (emailExists.rows[0]) {
+            res.sendStatus(409);
+            return;
+        }
 
         const body = {
             username,
@@ -20,21 +23,26 @@ export async function createUser(req: Request, res: Response): Promise<Response<
             password: encryptedPassword
         }
         await insertNewUser(body);
-        return res.sendStatus(201);
+        res.sendStatus(201);
+        return;
 
     } catch (err: unknown) {
         console.error(err);
-        return res.sendStatus(500);
+        res.sendStatus(500);
+        return;
     }
 }
 
-export async function signInUser(req: Request, res: Response): Promise<Response<any, Record<string, any>>> {
+export async function signInUser(req: Request, res: Response) {
     const email: string = stripHtml(req.body.email.trim()).result;
     const password: string = stripHtml(req.body.password.trim()).result;
 
     try {
         const user = await searchUser(email);
-        if (!user.rows[0]) { return res.sendStatus(404); }
+        if (!user.rows[0]) {
+            res.sendStatus(404);
+            return;
+        }
 
         const passwordCheck: boolean = bcrypt.compareSync(password, user.rows[0].password);
 
@@ -46,7 +54,8 @@ export async function signInUser(req: Request, res: Response): Promise<Response<
                     token: session.rows[0].token,
                     username: user.rows[0].username,
                 }
-                return res.status(200).send(body);
+                res.status(200).send(body);
+                return;
 
             } else {
                 const token: string = uuid();
@@ -56,88 +65,117 @@ export async function signInUser(req: Request, res: Response): Promise<Response<
                 }
                 await insertNewSession(body);
 
-                return res.status(200).send({
+                res.status(200).send({
                     token
                 });
+                return;
 
             }
         } else {
-            return res.sendStatus(401);
+            res.sendStatus(401);
+            return;
         }
     } catch (err: unknown) {
         console.error(err);
-        return res.sendStatus(500);
+        res.sendStatus(500);
+        return;
     }
 }
 
-export async function updateUsername(req: Request, res: Response): Promise<Response<any, Record<string, any>>> {
+export async function updateUsername(req: Request, res: Response) {
     const password: string = stripHtml(req.body.password.trim()).result;
     const username: string = stripHtml(req.body.username.trim()).result;
     const { authorization } = req.headers;
-    if (!authorization) { return res.sendStatus(401); }
+    if (!authorization) {
+        res.sendStatus(401);
+        return;
+    }
 
     const token: string = stripHtml(authorization.replace('Bearer ', '')).result;
 
     try {
         const user = await searchUserBySession(token);
-        if (!user.rows[0]) { return res.sendStatus(404); }
+        if (!user.rows[0]) {
+            res.sendStatus(404);
+            return;
+        }
 
         const passwordCheck: boolean = bcrypt.compareSync(password, user.rows[0].password);
 
         if (user && passwordCheck) {
             await changeUsername(username, user.rows[0].email);
-            return res.sendStatus(200);
+            res.sendStatus(200);
+            return;
         } else {
-            return res.sendStatus(401);
+            res.sendStatus(401);
+            return;
         }
 
     } catch (err: unknown) {
         console.error(err);
-        return res.sendStatus(500);
+        res.sendStatus(500);
+        return;
     }
 }
 
-export async function deleteUser(req: Request, res: Response): Promise<Response<any, Record<string, any>>> {
+export async function deleteUser(req: Request, res: Response) {
     const password: string = stripHtml(req.body.password.trim()).result;
     const { authorization } = req.headers;
-    if (!authorization) { return res.sendStatus(401); }
+    if (!authorization) {
+        res.sendStatus(401);
+        return;
+    }
 
     const token: string = stripHtml(authorization.replace('Bearer ', '')).result;
 
     try {
         const user = await searchUserBySession(token);
-        if (!user.rows[0]) { return res.sendStatus(404); }
+        if (!user.rows[0]) {
+            res.sendStatus(404);
+            return;
+        }
 
         const passwordCheck: boolean = bcrypt.compareSync(password, user.rows[0].password);
 
         if (user && passwordCheck) {
             await removeUser(user.rows[0].email);
-            return res.sendStatus(204);
+            res.sendStatus(204);
+            return;
         } else {
-            return res.sendStatus(401);
+            res.sendStatus(401);
+            return;
         }
 
     } catch (err: unknown) {
         console.error(err);
-        return res.sendStatus(500);
+        res.sendStatus(500);
+        return;
     }
 }
 
 export async function userStats(req: Request, res: Response) {
     const { authorization } = req.headers;
-    if (!authorization) { return res.sendStatus(401); }
+    if (!authorization) {
+        res.sendStatus(401);
+        return;
+    }
 
     const token: string = stripHtml(authorization.replace('Bearer ', '')).result;
 
     try {
         const user = await searchUserBySession(token);
-        if (!user.rows[0]) { return res.sendStatus(404); }
+        if (!user.rows[0]) {
+            res.sendStatus(404);
+            return;
+        }
 
         const userInfo = await getUserStats(user.rows[0].email);
-        return res.status(200).send(userInfo.rows[0]);
+        res.status(200).send(userInfo.rows[0]);
+        return;
 
     } catch (err: unknown) {
         console.error(err);
-        return res.sendStatus(500);
+        res.sendStatus(500);
+        return;
     }
 }
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { createUser, signInUser, updateUsername, deleteUser, userStats } from "../controllers/userController.js";
+import { userDeleteMiddleware, usernameUpdateMiddleware, userSignInMiddleware, userSignUpMiddleware } from "../middlewares/userSchemasMiddlewares.js";
 
 const userRouter = Router();
 
-userRouter.post("/user/signup", createUser);
-userRouter.post("/user/signin", signInUser);
-userRouter.put("/user", updateUsername);
-userRouter.delete("/user", deleteUser);
+userRouter.post("/signup", userSignUpMiddleware, createUser);
+userRouter.post("/signin", userSignInMiddleware, signInUser);
+userRouter.put("/user", usernameUpdateMiddleware, updateUsername);
+userRouter.delete("/user", userDeleteMiddleware, deleteUser);
 userRouter.get("/user", userStats);
 
 export default userRouter;
